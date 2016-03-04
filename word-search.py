8a96c120d7d88e6592e85463c93251a6f909608e
leafypixiestix <justincai.lps@gmail.com>
2016-03-04 09:37:21 -0700

    Added create puzzle function

--- word-search.py
+++ word-search.py
@@ -1,5 +1,6 @@
-import random
-
+import random     
+import string
+   
 def printArray(array):
     for r in array:
         for c in r:
@@ -161,7 +162,122 @@ def solve(array, words):
         else:
             solution.append(-1)    
         print word, coords
+
+#Pass the direction as string
+def passThrough(startingCoord, length, direction, intersection):
+    if direction == "east":
+        if startingCoord[0] == intersection[0]:
+            endingCoord = startingCoord[1] + length - 1
+            if (intersection[1] >= startingCoord[1]) and (intersection[1] <= endingCoord):
+                return True
+        return False
+    elif direction == "south":
+        if startingCoord[1] == intersection[1]:
+            endingCoord = startingCoord[0] + length - 1
+            if (intersection[0] >= startingCoord[0]) and (intersection[0] <= endingCoord):
+                return True
+        return False
+    elif direction == "ne":
+        endingCoord = [startingCoord[0] + length - 1, startingCoord[1] - length + 1]
+        x0 = startingCoord[0]
+        y0 = startingCoord[1]
+        xf = endingCoord[0]
+        yf = endingCoord[1]
+        xi = intersection[0]
+        xf = intersection[1]
+        if(x0 * (yf - yi) - y0 () / 2 == 0):
+            return True
+        return False
+    elif direction == "se":
+        return False
         
+def placeWord(coordinateArray, word, rows, columns, invalidCoords, puzzle):
+    for coord in coordinateArray:
+            r = range(4)
+            random.shuffle(r)
+            for i in r:
+                #east
+                if i == 0: 
+                    if (coord[1] + len(word) <= columns):
+                        valid = True
+                        for invalidCoord in invalidCoords:
+                            if(passThrough(coord, len(word), "east", invalidCoord)):
+                                valid = False
+                                break
+                        if valid:
+                            for j, char in enumerate(word):
+                                puzzle[coord[0]][coord[1] + j] = char 
+                            return
+                #south
+                elif i == 1:
+                    if (coord[0] + len(word) <= rows):
+                        valid = True
+                        for invalidCoord in invalidCoords:
+                            if(passThrough(coord, len(word), "west", invalidCoord)):
+                                valid = False
+                                break
+                        if valid:
+                            for j, char in enumerate(word):
+                                puzzle[coord[0] + j][coord[1]] = char 
+                            return
+                #ne
+                elif i == 2:
+                    if (coord[0] - len(word) >= -1) and (coord[1] + len(word) <= columns):
+                        valid = True
+                        for invalidCoord in invalidCoords:
+                            if(passThrough(coord, len(word), "east", invalidCoord)):
+                                valid = False
+                                break
+                        if valid:
+                            for j, char in enumerate(word):
+                                puzzle[coord[0] - j][coord[1] + j] = char 
+                            return
+                #se
+                else:
+                    if (coord[0] + len(word) <= columns) and (coord[1] + len(word) <= rows):
+                        valid = True
+                        for invalidCoord in invalidCoords:
+                            if(passThrough(coord, len(word), "east", invalidCoord)):
+                                valid = False
+                                break
+                        if valid:
+                            for j, char in enumerate(word):
+                                puzzle[coord[0] + j][coord[1] + j] = char 
+                            return
+        
+#Creates a word search with the a given array of words, rows, and columns
+def createPuzzle(words, rows, columns):
+    
+    puzzle = [['' for c in range(columns)] for r in range(rows)]
+    #Iterate through each word
+    for word in words:
+        #Create a list of invalid coordinates, the word placed in the puzzle CANNOT pass through these.
+        invalidCoords = []
+        for i, row in enumerate(puzzle):
+            for j, letter in enumerate(row):
+                if not letter == '' and not letter in word:
+                    invalidCoords.append([i,j])
+        #Randomly cycle through coordinates in the puzzle and attempt to place the word
+        coordinateArray = []
+        for r in range(rows):
+            for c in range(columns):
+                coordinateArray.append([r,c])
+        random.shuffle(coordinateArray)
+        for coord in invalidCoords:
+            coordinateArray.remove(coord)
+        placeWord(coordinateArray, word, rows, columns, invalidCoords, puzzle)
+    #Place random letters in the rest of the puzzle
+    for i, row in enumerate(puzzle):
+        for j, char in enumerate(row):
+            if char == '':
+                puzzle[i][j] = random.choice(string.ascii_lowercase)
+    return puzzle
+            
+                
 array = [['1','2','3','4'],['5','6','7','8'],['9','0','1','2'],['3','4','5','6']]
 printArray(array)
 solve(array, ['50' , '307', '34'])
+words = ["123", "456789", "123456789012345"]
+puzzle = createPuzzle(words, 15, 15)
+printArray(puzzle)
+solve(puzzle, words)
